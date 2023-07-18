efa71b1dc3762ee011e3ebd17b38cf9a36f47c9e
ThiagoAguirre <thiago.aguirre08@outlook.com>
2023-07-18 12:46:49 -0300

    nova tentativa

--- src/js/index.js
+++ src/js/index.js
@@ -7,71 +7,71 @@ trilhaSonora.volume = 0.04;
 
 const personagens = document.querySelectorAll(".personagem");
 personagens.forEach((personagem) => {
-    personagem.addEventListener("click", () => {
-        if (selecionada) {
-            selecionada.classList.remove("clicado");
-        }
+  personagem.addEventListener("click", () => {
+    if (selecionada) {
+      selecionada.classList.remove("clicado");
+    }
 
-        removerSelecaoDoPersonagem();
+    removerSelecaoDoPersonagem();
 
-        personagem.classList.add("selecionado");
-        personagem.classList.add("clicado");
+    personagem.classList.add("selecionado");
+    personagem.classList.add("clicado");
 
-        selecionada = personagem;
+    selecionada = personagem;
 
-        alterarImagemPersonagemSelecionado(personagem);
+    alterarImagemPersonagemSelecionado(personagem);
 
-        alterarNomePersonagemSelecionado(personagem);
+    alterarNomePersonagemSelecionado(personagem);
 
-        alterarDescricaoPersonagem(personagem);
+    alterarDescricaoPersonagem(personagem);
 
-        espadaSelecao.load();
-        espadaSelecao.play();
-    });
+    espadaSelecao.load();
+    espadaSelecao.play();
+  });
 });
 
 window.addEventListener("load", () => {
-    trilhaSonora.play();
+  trilhaSonora.play();
 });
 
 function alterarDescricaoPersonagem(personagem) {
-    const descricaoPersonagem = document.getElementById("descricao-personagem");
-    descricaoPersonagem.classList.remove("fade-in");
-    descricaoPersonagem.classList.add("fade-out");
+  const descricaoPersonagem = document.getElementById("descricao-personagem");
+  descricaoPersonagem.classList.remove("fade-in");
+  descricaoPersonagem.classList.add("fade-out");
 
-    setTimeout(() => {
-        descricaoPersonagem.innerText = personagem.getAttribute("data-description");
-        descricaoPersonagem.classList.remove("fade-out");
-        descricaoPersonagem.classList.add("fade-in");
-    }, 300);
+  setTimeout(() => {
+    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+    descricaoPersonagem.classList.remove("fade-out");
+    descricaoPersonagem.classList.add("fade-in");
+  }, 300);
 }
 
 function alterarNomePersonagemSelecionado(personagem) {
-    const nomePersonagem = document.getElementById("nome-personagem");
-    nomePersonagem.classList.remove("fade-in");
-    nomePersonagem.classList.add("fade-out");
+  const nomePersonagem = document.getElementById("nome-personagem");
+  nomePersonagem.classList.remove("fade-in");
+  nomePersonagem.classList.add("fade-out");
 
-    setTimeout(() => {
-        nomePersonagem.innerText = personagem.getAttribute("data-name");
-        nomePersonagem.classList.remove("fade-out");
-        nomePersonagem.classList.add("fade-in");
-    }, 300);
+  setTimeout(() => {
+    nomePersonagem.innerText = personagem.getAttribute("data-name");
+    nomePersonagem.classList.remove("fade-out");
+    nomePersonagem.classList.add("fade-in");
+  }, 300);
 }
 
 function alterarImagemPersonagemSelecionado(personagem) {
-    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
-    const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.classList.remove("fade-in");
-    imagemPersonagemGrande.classList.add("fade-out");
+  const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+  const idPersonagem = personagem.attributes.id.value;
+  imagemPersonagemGrande.classList.remove("fade-in");
+  imagemPersonagemGrande.classList.add("fade-out");
 
-    setTimeout(() => {
-        imagemPersonagemGrande.src = `src/image/card-${idPersonagem}.png`;
-        imagemPersonagemGrande.classList.remove("fade-out");
-        imagemPersonagemGrande.classList.add("fade-in");
-    }, 300);
+  setTimeout(() => {
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+    imagemPersonagemGrande.classList.remove("fade-out");
+    imagemPersonagemGrande.classList.add("fade-in");
+  }, 300);
 }
 
 function removerSelecaoDoPersonagem() {
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+  const personagemSelecionado = document.querySelector(".selecionado");
+  personagemSelecionado.classList.remove("selecionado");
 }
